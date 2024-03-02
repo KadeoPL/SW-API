6118674c5f68aac76af89d9865ffb08542a49586
Krzysztof Stopka <stopkakrzysztof1@gmail.com>
2024-03-02 23:02:06 +0100

    add vite project

---
+++ SWAPI/personclass.js
@@ -0,0 +1,9 @@
+class Person {
+    constructor(name, height, homeworld) {
+        this.name = name;
+        this.height = height;
+        this.homeworld = homeworld;
+    }
+}
+
+export {Person};
