c886c5532162d759f5e6db8e3bf5b7a2f955ce9d
Krzysztof Stopka <stopkakrzysztof1@gmail.com>
2024-03-02 23:12:18 +0100

    Updates

--- SWAPI/personclass.js
+++
@@ -1,9 +0,0 @@
-class Person {
-    constructor(name, height, homeworld) {
-        this.name = name;
-        this.height = height;
-        this.homeworld = homeworld;
-    }
-}
-
-export {Person};
